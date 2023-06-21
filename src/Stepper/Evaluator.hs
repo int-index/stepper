5fd9afe643d36f15e96d7bffc944d0cc81509fb6
Vladislav Zavialov <vlad.z.4096@gmail.com>
2023-06-21 21:19:32 +0200

    Jump to ref in evaluation

--- src/Stepper/Evaluator.hs
+++ src/Stepper/Evaluator.hs
@@ -1,20 +1,21 @@
 module Stepper.Evaluator where
 
+import Data.Map (Map)
+import qualified Data.Map as Map
+
 import Stepper.Syntax.Basic
 import Stepper.Syntax.Scoped
 
 evalstep :: Module -> TopId -> Maybe Module  -- Nothing <=> nothing to reduce
-evalstep (Mod bs) entryPoint = do
-  b <- lookupTopBinding entryPoint bs
-  case evalstepTopBinding b of
-    Nothing -> Nothing
-    Just b' -> Just (Mod (setTopBinding b' bs))
-
-lookupTopBinding :: TopId -> [TopBinding] -> Maybe TopBinding
-lookupTopBinding _ [] = Nothing
-lookupTopBinding name (b@(TopBind name' _) : bs)
-  | name == name' = Just b
-  | otherwise = lookupTopBinding name bs
+evalstep (Mod bs) = go
+  where
+    env = Map.fromList [ (name, b) | b@(TopBind name _) <- bs ]
+    go name = do
+      b <- Map.lookup name env
+      case evalstepTopBinding env b of
+        Stuck -> Nothing
+        Update b' -> Just (Mod (setTopBinding b' bs))
+        Jump name' -> go name'
 
 setTopBinding :: TopBinding -> [TopBinding] -> [TopBinding]
 setTopBinding _ [] = []
@@ -22,16 +23,60 @@ setTopBinding b@(TopBind name _) (b'@(TopBind name' _) : bs)
   | name == name' = b : bs
   | otherwise = b' : setTopBinding b bs
 
-evalstepTopBinding :: TopBinding -> Maybe TopBinding
-evalstepTopBinding (TopBind name e) = do
-  e' <- evalstepExpr e
-  return (TopBind name e')
+data Outcome =
+    Stuck
+  | Update TopBinding
+  | Jump TopId
 
-evalstepExpr :: ClosedExpr TopId -> Maybe (ClosedExpr TopId)
-evalstepExpr (PrimE primop :@ lhs :@ rhs)
+orIfStuck :: Outcome -> Outcome -> Outcome
+Stuck `orIfStuck` r = r
+r `orIfStuck` _ = r
+
+evalstepTopBinding :: TopEnv -> TopBinding -> Outcome
+evalstepTopBinding env (TopBind name e) = evalstepExpr env (TopBind name) e
+
+type ExprCtx a = Expr TopId a -> TopBinding
+
+type TopEnv = Map TopId TopBinding
+
+isWHNF :: Expr TopId ctx -> Bool
+isWHNF e =
+  case e of
+    RefE{}  -> False
+    VarE{}  -> True
+    ConE{}  -> True
+    LitE{}  -> True
+    PrimE{} -> True
+    LamE{}  -> True
+    _ :@ _  -> False
+    CaseE{} -> False
+    LetE{}  -> False
+
+evalstepExpr :: TopEnv -> ExprCtx ctx -> Expr TopId ctx -> Outcome
+evalstepExpr env ctx (PrimE primop :@ lhs :@ rhs)
   | Just f <- matchIntegerBinOp primop
-  = evalstepIntegerBinOp f primop lhs rhs
-evalstepExpr _ = Nothing
+  = evalstepIntegerBinOp ctx f lhs rhs `orIfStuck`
+    evalstepExpr env (\lhs' -> ctx (PrimE primop :@ lhs' :@ rhs)) lhs `orIfStuck`
+    evalstepExpr env (\rhs' -> ctx (PrimE primop :@ lhs :@ rhs')) rhs
+evalstepExpr env ctx (RefE ref)
+  | Just (TopBind _ e) <- Map.lookup ref env
+  , isWHNF e
+  = Update (ctx (extendExprCtx e))
+  | otherwise = Jump ref
+-- evalstepEpxr ctx (CaseE (LitE _) bs) = ...
+-- evalstepExpr ctx (CaseE e bs) = evalstepExpr (\e' -> ctx (CaseE e' bs)) e
+evalstepExpr _ _ _ = Stuck
+
+evalstepIntegerBinOp ::
+  ExprCtx ctx ->
+  (Integer -> Integer -> Integer) ->
+  Expr TopId ctx ->
+  Expr TopId ctx ->
+  Outcome
+evalstepIntegerBinOp ctx f lhs rhs
+  | LitE (IntL a) <- lhs, LitE (IntL b) <- rhs
+  = Update (ctx (LitE (IntL (f a b))))
+  | otherwise = Stuck
 
 matchIntegerBinOp :: PrimOp -> Maybe (Integer -> Integer -> Integer)
 matchIntegerBinOp primop =
@@ -41,13 +86,3 @@ matchIntegerBinOp primop =
     Integer_mul -> Just (*)
     Integer_div -> Just div
     _ -> Nothing
-
-evalstepIntegerBinOp ::
-  (Integer -> Integer -> Integer) ->
-  PrimOp -> ClosedExpr TopId -> ClosedExpr TopId -> Maybe (ClosedExpr TopId)
-evalstepIntegerBinOp f primop lhs rhs
-  | Just lhs' <- evalstepExpr lhs = Just (PrimE primop :@ lhs' :@ rhs)
-  | Just rhs' <- evalstepExpr rhs = Just (PrimE primop :@ lhs :@ rhs')
-  | LitE (IntL a) <- lhs, LitE (IntL b) <- rhs =
-      Just (LitE (IntL (f a b)))
-  | otherwise = Nothing
--- src/Stepper/Syntax/Scoped.hs
+++ src/Stepper/Syntax/Scoped.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+
 module Stepper.Syntax.Scoped where
 
 import Data.Kind
@@ -45,6 +47,23 @@ infixl 2 :@
 
 deriving instance Show ref => Show (Expr ref ctx)
 
+extendExprCtx :: forall ctx' ctx ref. Expr ref ctx -> Expr ref (ctx ++ ctx')
+extendExprCtx e0 =
+  case e0 of
+    RefE ref -> RefE ref
+    VarE i -> VarE (extendIndexBase @ctx' i)
+    ConE con -> ConE con
+    LitE lit -> LitE lit
+    PrimE primop -> PrimE primop
+    LamE varBndr e -> LamE varBndr (extendExprCtx @ctx' e)
+    e1 :@ e2 -> extendExprCtx @ctx' e1 :@ extendExprCtx @ctx' e2
+    CaseE e bs -> CaseE (extendExprCtx @ctx' e) (map (extendBranchCtx @ctx') bs)
+    LetE (bs :: HList f out) e ->
+      assocListAppend @_ @ctx @ctx' bs $
+      LetE
+        (hmap (extendBindingCtx @ctx' @ctx @out) bs)
+        (extendExprCtx @ctx' e)
+
 type Branch :: Type -> [VarInfo] -> Type
 data Branch ref ctx where
   (:->) :: Pat out -> Expr ref (out ++ ctx) -> Branch ref ctx
@@ -52,6 +71,13 @@ data Branch ref ctx where
 infix 0 :->
 
 deriving instance Show ref => Show (Branch ref ctx)
+
+extendBranchCtx :: forall ctx' ref ctx. Branch ref ctx -> Branch ref (ctx ++ ctx')
+extendBranchCtx (p :-> e) = p :-> e'
+  where
+    e' =
+      assocListAppend @_ @ctx @ctx' (patVarBndrs p) $
+      extendExprCtx @ctx' e
 
 type Pat :: [VarInfo] -> Type
 data Pat out where
@@ -62,11 +88,22 @@ data Pat out where
 
 deriving instance Show (Pat out)
 
+patVarBndrs :: Pat out -> HList VarBndr out
+patVarBndrs p =
+  case p of
+    VarP varBndr -> varBndr :& HNil
+    ConP _ varBndrs -> varBndrs
+    LitP _ -> HNil
+    WildP -> HNil
+
 type Binding :: Type -> [VarInfo] -> VarInfo -> Type
 data Binding ref ctx v where
   Bind :: VarBndr v -> Expr ref ctx -> Binding ref ctx v
 
 deriving instance Show ref => Show (Binding ref ctx v)
+
+extendBindingCtx :: forall ctx' ctx out ref v. Binding ref (out ++ ctx) v -> Binding ref ((out ++ ctx) ++ ctx') v
+extendBindingCtx (Bind varBndr e) = Bind varBndr (extendExprCtx @ctx' e)
 
 getBindingVarBndr :: Binding ref ctx v -> VarBndr v
 getBindingVarBndr (Bind varBndr _) = varBndr
